cc9cc0ed0fb183befabbdc8d6e314102ede37ea2
Joan <jgose2001@gmail.com>
2024-07-11 13:18:24 +0200

    paginated list

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,12 +1,13 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Put,
-  Delete,
-  Param,
-  Body,
-  NotFoundException,
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Param,
+    Body,
+    NotFoundException,
+    Query,
 } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
 import { ApiTags, ApiOperation } from "@nestjs/swagger";
@@ -16,55 +17,59 @@ import { UserPipe } from "../flow/user.pipe";
 @ApiTags("users")
 @Controller("users")
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) {}
 
-  // GET /users
-  @Get()
-  @ApiOperation({ summary: "Get all users" })
-  async findAll() {
-      return this.userService.findAll(); // Retrieves all users from the database
-  }
+    // GET /users
+    @Get()
+    @ApiOperation({ summary: "Get all users" })
+    async list(
+        @Query("page") page: number = 1,
+        @Query("pageSize") pageSize: number = 10,
+        @Query("filters") filters?: string
+    ) {
+        return this.userService.list(page, pageSize, filters); // Retrieves all users with pagination
+    }
 
-  // GET /users/:id
-  @Get(":id")
-  @ApiOperation({ summary: "Get a user by ID" })
-  async findOne(@Param("id") id: string) {
-      const user = await this.userService.findOne(id);
-      if (!user) {
-          throw new NotFoundException(`User with id ${id} not found`);
-      }
-      return user; // Returns a specific user by ID
-  }
+    // GET /users/:id
+    @Get(":id")
+    @ApiOperation({ summary: "Get a user by ID" })
+    async findOne(@Param("id") id: string) {
+        const user = await this.userService.findOne(id);
+        if (!user) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+        return user; // Returns a specific user by ID
+    }
 
-  // POST /users
-  @Post()
-  @ApiOperation({ summary: "Create a new user" })
-  async create(@Body(UserPipe) data: Prisma.UserCreateInput) {
-      return this.userService.create(data); // Creates a new user with the provided data
-  }
+    // POST /users
+    @Post()
+    @ApiOperation({ summary: "Create a new user" })
+    async create(@Body(UserPipe) data: Prisma.UserCreateInput) {
+        return this.userService.create(data); // Creates a new user with the provided data
+    }
 
-  // PUT /users/:id
-  @Put(":id")
-  @ApiOperation({ summary: "Update an existing user by ID" })
-  async update(
-      @Param("id") id: string,
-      @Body() data: Prisma.UserUpdateInput
-  ) {
-      const user = await this.userService.update(id, data);
-      if (!user) {
-          throw new NotFoundException(`User with id ${id} not found`);
-      }
-      return user; // Updates an existing user by ID with the provided data
-  }
+    // PUT /users/:id
+    @Put(":id")
+    @ApiOperation({ summary: "Update an existing user by ID" })
+    async update(
+        @Param("id") id: string,
+        @Body() data: Prisma.UserUpdateInput
+    ) {
+        const user = await this.userService.update(id, data);
+        if (!user) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+        return user; // Updates an existing user by ID with the provided data
+    }
 
-  // DELETE /users/:id
-  @Delete(":id")
-  @ApiOperation({ summary: "Delete a user by ID" })
-  async delete(@Param("id") id: string) {
-      const deletedUser = await this.userService.delete(id);
-      if (!deletedUser) {
-          throw new NotFoundException(`User with id ${id} not found`);
-      }
-      return deletedUser; // Deletes a user by ID
-  }
+    // DELETE /users/:id
+    @Delete(":id")
+    @ApiOperation({ summary: "Delete a user by ID" })
+    async delete(@Param("id") id: string) {
+        const deletedUser = await this.userService.delete(id);
+        if (!deletedUser) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+        return deletedUser; // Deletes a user by ID
+    }
 }
--- src/modules/users/services/user.service.ts
+++ src/modules/users/services/user.service.ts
@@ -1,12 +1,46 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
 import { PrismaService } from "../../common";
 
 @Injectable()
 export class UserService {
     public constructor(private readonly prismaService: PrismaService) {}
-    async findAll() {
-        return this.prismaService.user.findMany();
+    async list(page: number, pageSize: number, filters?: string) {
+        // Calculate skip and take for pagination
+        const skip = (page - 1) * pageSize;
+        const take = Number(pageSize);
+        let where: Prisma.UserWhereInput = {};
+
+        // Parse filters if present
+        if (filters) {
+            try {
+                where = JSON.parse(filters);
+            } catch (error) {
+                throw new BadRequestException(
+                    "Invalid JSON format for filters"
+                );
+            }
+        }
+
+        // Perform transaction to fetch users and total count
+        const [users, total] = await this.prismaService.$transaction([
+            this.prismaService.user.findMany({
+                where,
+                skip,
+                take,
+            }),
+            this.prismaService.user.count({
+                where,
+            }),
+        ]);
+
+        // Return paginated results
+        return {
+            data: users,
+            total,
+            page,
+            pageSize,
+        };
     }
 
     async findOne(id: string) {
